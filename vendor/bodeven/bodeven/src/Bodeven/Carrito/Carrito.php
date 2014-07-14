62dff98ec7cea8bfc372b6a9dced31e241efcc30
Administrador <ramon.calle.88@gmail.com>
2014-07-14 00:19:16 -0430

    Carpeta de dependencias agregada.

---
+++ vendor/bodeven/bodeven/src/Bodeven/Carrito/Carrito.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by Ramon Serrano
+ * User: serrano
+ * Date: 12/07/14
+ * Time: 06:52 PM
+ */
+
+namespace Bodeven\Carrito;
+
+
+class Carrito {
+
+}
---
+++ vendor/bodeven/bodeven/src/Bodeven/Carrito/Producto.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: serrano
+ * Date: 12/07/14
+ * Time: 06:53 PM
+ */
+
+namespace Bodeven\Carrito;
+
+
+class Producto {
+    var $codigo;
+    var $nombre;
+    var $precio;
+} 
