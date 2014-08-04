cd5deadbf532672fcb9d8c5282cc18f2ad3fd671
Ramón Serrano <ramon.calle.88@gmail.com>
2014-08-04 16:47:30 -0430

    Modificados la configuracion de la aplicacion y de la consola de comandos.

--- src/app.php
+++ src/app.php
@@ -1,32 +1,31 @@
 <?php
 
-/*
- * This file is part of the CRUD Admin Generator project.
- *
- * Author: Jon Segador <jonseg@gmail.com>
- * Web: http://crud-admin-generator.com
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+# Autocargador del framework
+require_once __DIR__ . '/../vendor/autoload.php';
 
-# Proveedor de usuario
-require_once __DIR__ . '/../src/UserProvider.php';
+use Silex\Application,
+    Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
 
-use Silex\Application;
-
+# Objeto de la aplicacion Silex
 $app = new Application();
 
+# Provedor Twig para las vistas
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/../web/views',
+    'twig.path' => __DIR__ . '/../web/views',
 ));
+# Proveedor de Formularios
 $app->register(new Silex\Provider\FormServiceProvider());
+# Proveedor de Traducciones
 $app->register(new Silex\Provider\TranslationServiceProvider(), array(
     'translator.messages' => array(),
 ));
+# Provedor para validaciones
 $app->register(new Silex\Provider\ValidatorServiceProvider());
+# Proveedor para las urls
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+# Proveedor para el uso de variables de sesion
 $app->register(new Silex\Provider\SessionServiceProvider());
+# Proveedor de seguridad de acceso a las url
 $app->register(new Silex\Provider\SecurityServiceProvider(), array(
     'security.firewalls' => array(
         'login_path' => array(
@@ -45,33 +44,39 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
                 'invalidate_session' => false
             ),
             'users' => $app->share(function($app) {
-                return new UserProvider($app['db']);
-            }),
+                    return new Bodeven\UserProvider($app['db']);
+                }),
         )
     ),
     'security.access_rules' => array(
         array('^/login$', 'IS_AUTHENTICATED_ANONYMOUSLY'),
-        array('^/admin/perfil', 'ROLE_SUPER_ADMIN'),
-        array('^/admin/usuario', 'ROLE_SUPER_ADMIN'),
-        array('^/admin', array('ROLE_SUPER_ADMIN', 'ROLE_ADMIN')),
-        array('^/admin', 'ROLE_USER')
+        array('^/admin/perfils', 'ROLE_Programador'),
+        array('^/admin/users', array('ROLE_Programador', 'ROLE_Administrador')),
+        array('^/admin', array('ROLE_Programador', 'ROLE_Administrador')),
+        array('^/admin', 'ROLE_Usuario')
+    ),
+    'security.encoder.digest' => $app->share(function($app) {
+            return new MessageDigestPasswordEncoder('sha512');
+        })
+));
+# Proveedor de doctrine para base de datos
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'dbs.options' => array(
+        'db' => array(
+            'driver' => 'pdo_mysql',
+            'dbname' => 'tania_serrano',
+            'host' => 'localhost',
+            'user' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ),
     )
 ));
-$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+#Provedor de swiftmailer
+$app->register(new Silex\Provider\SwiftmailerServiceProvider());
+#Proveedor de carrito de compras
+$app->register(new Bodeven\SilexProvider\CartServiceProvider());
 
-        'dbs.options' => array(
-            'db' => array(
-                'driver'   => 'pdo_mysql',
-                'dbname'   => 'tania_serrano',
-                'host'     => '127.0.0.1',
-                'user'     => 'root',
-                'password' => '',
-                'charset'  => 'utf8',
-            ),
-        )
-));
-
-$app['asset_path'] = 'http://localhost/bodeven2/web/resources';
 $app['debug'] = true;
 
 return $app;
